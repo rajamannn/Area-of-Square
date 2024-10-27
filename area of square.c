9fda0b7a8457ea4c64f1d3d7caff5913c8b8563c
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-10-27 21:12:53 +0530

    Update area of square.c

--- area of square.c
+++ area of square.c
@@ -1,4 +1,4 @@
-/*#include<stdio.h>
+#include<stdio.h>
 
 int main()
 {
@@ -8,22 +8,3 @@ int main()
 area = side * side;
   printf("Area of a square= %d\n", area);
  return 0;
-
-}*/
-#include<stdio.h>
-
-int main()
- {
-  int a=3,b=5,c=1;
-  if (a<b)
-  {
-    printf("First number");
-  } else if (b<c)
-   {
-    printf("Second number ");
-  } else
-   {
-    printf("Third Number");
-  }
-  return 0;
-}
