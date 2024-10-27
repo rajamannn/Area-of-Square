0224d5690010bbc141226b7142f7f97eb71e0a7f
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-10-27 21:11:33 +0530

    Add files via upload

---
+++ area of square.c
@@ -0,0 +1,29 @@
+/*#include<stdio.h>
+
+int main()
+{
+  int side= 5;
+  int area;
+
+area = side * side;
+  printf("Area of a square= %d\n", area);
+ return 0;
+
+}*/
+#include<stdio.h>
+
+int main()
+ {
+  int a=3,b=5,c=1;
+  if (a<b)
+  {
+    printf("First number");
+  } else if (b<c)
+   {
+    printf("Second number ");
+  } else
+   {
+    printf("Third Number");
+  }
+  return 0;
+}
